c2281fa617729635818a247c42f7c19797199f87
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-04 15:36:29 -0500

    Scope

--- assets/index.js
+++ assets/index.js
@@ -1,30 +1,7 @@
+import MediaPlayer from './MediaPlayer.js'
 
-      class MediaPlayer {
-        constructor ({element}){
-          this.media = element;
+const video = document.querySelector('video');
+const player = new MediaPlayer({element:video });
 
-        }
-
-        play (){
-          this.media.play();
-        }
-
-        pause(){
-          this.media.pause();
-        }
-
-        togglePlay(){
-          if (this.media.paused) {
-              this.play();
-          } else {
-              this.pause();
-          }
-        }
-      }
-
-
-      const video = document.querySelector('video');
-      const player = new MediaPlayer({element:video });
-
-      const button = document.querySelector('button');
-      button.onclick = () => player.togglePlay();
+const button = document.querySelector('button');
+button.onclick = () => player.togglePlay();
