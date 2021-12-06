a291898d7dcfa84a77a094478ffbe4750fd746ca
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-05 20:35:54 -0500

    Service Workers

--- assets/index.js
+++ assets/index.js
@@ -8,5 +8,20 @@ const player = new MediaPlayer({config:{element:video
     ,plugins: [new AutoPlay(), new AutoPause()]
 } });
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
+
+
+if ('serviceWorker' in navigator)
+{
+    navigator.serviceWorker.register('/sw.js').catch(error => console.log(error.message))
+}
--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,12 +2,28 @@ class AutoPause {
     constructor(){
         this.threshold =0.25
         this.handlerIntersection= this.handlerIntersection.bind(this)
+        this.handleVisibilityChange= this.handleVisibilityChange.bind(this)
+
     }
 
     run(player){
         this.player=player
         const observer = new IntersectionObserver(this.handlerIntersection,{threshold:this.threshold})
         observer.observe(this.player.media);
+        document.addEventListener('visibilitychange',this.handleVisibilityChange);
+    }
+
+
+
+
+    handleVisibilityChange(){
+        const isVisible = document.visibilityState==='visible';
+        if (isVisible){
+            this.player.play()
+        }
+        else{
+            this.player.pause()
+        }
     }
 
     handlerIntersection(entries){
